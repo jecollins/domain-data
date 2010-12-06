3e0d296d5de4f6681b4d6c6edcce1c3746c0c8d0
ddauer <ddauer@gmail.com>
2010-12-06 22:46:05 +0100

    Moved org.powertac.common package to this git repo
    Moved org.powertac.common package to this git repo. Please note that the package IS NOT YET ready to be built by itself. Do not report any build issues.

---
+++ src/main/java/org/powertac/common/commands/RevokeTariffCommand.java
@@ -0,0 +1,6 @@
+package org.powertac.common.commands;
+
+public class RevokeTariffCommand {
+  private String authToken;
+  private Long tariffId;
+}
---
+++ src/main/java/org/powertac/common/interfaces/Timeslot.java
@@ -0,0 +1,10 @@
+package org.powertac.common.interfaces;
+
+/**
+ * Marker Interface used to denote Timeslot implementations
+ *
+ * @author Carsten Block
+ * @version 1.0, Date: 01.12.10
+ */
+public interface Timeslot {
+}
