27bd34b8a7252128f7ac882104b0830f82efc732
ddauer <ddauer@gmail.com>
2010-12-15 20:39:15 +0100

    Updated CompetitionStatus enum

--- src/main/java/org/powertac/common/enumerations/CompetitionStatus.java
+++ src/main/java/org/powertac/common/enumerations/CompetitionStatus.java
@@ -1,13 +1,50 @@
+/*
+ * Copyright 2009-2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.powertac.common.enumerations;
 
+/**
+ * Describes the status of a competition
+ */
 public enum CompetitionStatus {
-  Created(1),
+  /**
+   * The competition was created but there is no configuration available from either a (remote) file or database
+   */
+  Idle(1),
+  /**
+   * Once a configuration has been read, the competition is scheduled
+   */
   Scheduled(2),
+  /**
+   * All modules have been given the opportunity to initialize themselves. The initialization process
+   * should load large data files or fetch data from remote servers
+   */
   Initialized(3),
-  Ready(4),
-  Running(5),
-  Finished(6),
-  Interrupted(7);
+  /**
+   * The competition is running
+   */
+  Running(4),
+  /**
+   * The competition is finished
+   */
+  Finished(5),
+  /**
+   * The competition has been interrupted (i.e. paused or cancelled)
+   */
+  Interrupted(6);
 
   private final int idVal;
 
